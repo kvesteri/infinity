4898283dfb2c6b0543cef0dbbf7dc70ef87f82f7
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-01-05 12:04:30 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,75 @@
+"""
+infinity
+--------
+
+All-in-one infinity value for Python. Can be compared to any object.
+"""
+from setuptools import setup, Command, find_packages
+import subprocess
+import sys
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        sys.exit(errno)
+
+
+PY3 = sys.version_info[0] == 3
+
+
+extras_require = {
+    'test': [
+        'pytest==2.2.3',
+        'Pygments>=1.2',
+    ],
+}
+
+
+# Add all optional dependencies to testing requirements.
+for name, requirements in extras_require.items():
+    if name != 'test':
+        extras_require['test'] += requirements
+
+
+setup(
+    name='infinity',
+    version='0.1',
+    url='https://github.com/kvesteri/infinity',
+    license='BSD',
+    author='Konsta Vesterinen',
+    author_email='konsta@fastmonkeys.com',
+    description=(
+        'All-in-one infinity value for Python. Can be compared to any object.'
+    ),
+    long_description=__doc__,
+    packages=find_packages('.'),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    dependency_links=[],
+    install_requires=[
+        'six',
+        'total_ordering>=0.1'
+        if sys.version_info[0] == 2 and sys.version_info[1] < 7 else ''
+    ],
+    extras_require=extras_require,
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
